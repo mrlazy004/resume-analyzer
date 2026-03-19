828b58e034bb48065f445a990e0ad5093991757d
mrlazy004 <mrlazy004@gmail.com>
2026-03-19 22:44:01 +0530

    remove api key add env

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api.groq.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        headers: {
-          'Authorization': 'Bearer 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'
-        }
-      }
-    }
-  }
 })
